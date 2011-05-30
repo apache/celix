81f87c093fa5ee5a7e4c33774267ff6561ac4027
Alexander Broekhuis <abroekhuis@apache.org>
2011-05-30 09:41:29 +0000

    Added doxygen target to the doc target of the make files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/celix/trunk@1129089 13f79535-47bb-0310-9956-ffa450edef68

--- framework/private/include/celix_errno.h
+++ framework/private/include/celix_errno.h
@@ -4,20 +4,48 @@
  *  Created on: Feb 15, 2011
  *      Author: alexanderb
  */
-
+/*!
+  \file
+  \brief Error codes
+  \defgroup framework Celix Framework
+ */
 #ifndef CELIX_ERRNO_H_
 #define CELIX_ERRNO_H_
 
 #include <errno.h>
 
+/*!
+ * \defgroup celix_errno Error Codes
+ * \ingroup framework
+ * \{
+ */
+
+/*!
+ * Status type returned by all functions in Celix
+ */
 typedef int celix_status_t;
 
+/*!
+ * Error code indicating successful execution of the function.
+ */
 #define CELIX_SUCCESS 0
 
+/*!
+ * Starting point for Celix errors.
+ */
 #define CELIX_START_ERROR 20000
 
+/*!
+ * Exception indicating a problem with a bundle
+ */
 #define CELIX_BUNDLE_EXCEPTION (CELIX_START_ERROR + 1)
+/*!
+ * Invalid bundle context is used
+ */
 #define CELIX_INVALID_BUNDLE_CONTEXT (CELIX_START_ERROR + 2)
+/*!
+ * Argument is not correct
+ */
 #define CELIX_ILLEGAL_ARGUMENT (CELIX_START_ERROR + 3)
 #define CELIX_INVALID_SYNTAX (CELIX_START_ERROR + 4)
 #define CELIX_FRAMEWORK_SHUTDOWN (CELIX_START_ERROR + 5)
@@ -26,5 +54,9 @@ typedef int celix_status_t;
 #define CELIX_FILE_IO_EXCEPTION (CELIX_START_ERROR + 8)
 
 #define CELIX_ENOMEM ENOMEM
+
+/**
+ * \}
+ */
 
 #endif /* CELIX_ERRNO_H_ */
