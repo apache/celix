c225e93bab8f37b227554c92e03d4289bd5124dd
Alexander Broekhuis <abroekhuis@apache.org>
2012-06-24 15:16:18 +0000

    Project Structure
    Updated the project dependencies to use a single macro for setting build options.
    Dependencies are not set using build options, but using global variables. This makes it possible to disable sub project (and their dependencies).
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/celix/trunk@1353283 13f79535-47bb-0310-9956-ffa450edef68

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,10 +1,4 @@
-# dep_opt(name "descr" default DEPS a b c
-#option(BUILD_DEVICE_ACCESS "Option to enable building the Device Access Service bundles" OFF)
-#if(BUILD_DEVICE_ACCESS)
-#    get_property(log_doc CACHE BUILD_LOG_SERVICE PROPERTY HELPSTRING)
-#    set(BUILD_LOG_SERVICE "ON" CACHE BOOL "c" FORCE)
-
-MACRO(dep_opt)
+MACRO(celix_subproject)
     PARSE_ARGUMENTS(OPTION "DEPS" "" ${ARGN})
     LIST(GET OPTION_DEFAULT_ARGS 0 OPTION_NAME)
     LIST(GET OPTION_DEFAULT_ARGS 1 OPTION_DESCRIPTION)
@@ -15,12 +9,20 @@ MACRO(dep_opt)
     
     option(${NAME} "${OPTION_DESCRIPTION}" ${OPTION_DEFAULT})
     
-    IF (${NAME})
+    get_property(BUILD GLOBAL PROPERTY ${NAME}_INTERNAL)
+    if (NOT DEFINED BUILD)
+        set(BUILD "OFF")
+    endif (NOT DEFINED BUILD)
+    
+    IF (${NAME} OR ${BUILD})
+        set(${OPTION_NAME} "ON")
+    
         FOREACH (DEP ${OPTION_DEPS})
             string(TOUPPER ${DEP} UC_DEP)
             set(DEP_NAME "BUILD_${UC_DEP}")
-            get_property(doc_string CACHE ${DEP_NAME} PROPERTY HELPSTRING)
-            set(${DEP_NAME} "ON" CACHE BOOL "${doc_string}" FORCE)
+            set_property(GLOBAL PROPERTY ${DEP_NAME}_INTERNAL "ON")
         ENDFOREACH (DEP)
-    ENDIF (${NAME})
-ENDMACRO(dep_opt)
+    ELSE (${NAME} OR ${BUILD})
+        set(${OPTION_NAME} "OFF")
+    ENDIF (${NAME} OR ${BUILD})
+ENDMACRO(celix_subproject)
