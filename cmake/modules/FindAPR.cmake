4ad963c8d7ee44d8f1c58b64a76522c300f7f856
Alexander Broekhuis <abroekhuis@apache.org>
2013-09-19 14:08:09 +0000

    CELIX-71: Applied patch
    git-svn-id: https://svn.apache.org/repos/asf/incubator/celix/trunk@1524743 13f79535-47bb-0310-9956-ffa450edef68

--- cmake/modules/FindAPR.cmake
+++ cmake/modules/FindAPR.cmake
@@ -31,12 +31,13 @@ FIND_PATH(APR_INCLUDE_DIR apr.h
 /usr/local/include/apr-1.0
 /usr/include/apr-1
 /usr/include/apr-1.0
+/usr/local/apr/include/apr-1
 )
 
 SET(APR_NAMES ${APR_NAMES} apr-1)
 FIND_LIBRARY(APR_LIBRARY
   NAMES ${APR_NAMES}
-  PATHS /usr/lib /usr/local/lib
+  PATHS /usr/lib /usr/local/lib /usr/local/apr/lib
   )
 
 IF (APR_LIBRARY AND APR_INCLUDE_DIR)
@@ -73,12 +74,13 @@ FIND_PATH(APRUTIL_INCLUDE_DIR apu.h
 /usr/local/include/apr-1.0
 /usr/include/apr-1
 /usr/include/apr-1.0
+/usr/local/apr/include/apr-1
 )
 
 SET(APRUTIL_NAMES ${APRUTIL_NAMES} aprutil-1)
 FIND_LIBRARY(APRUTIL_LIBRARY
   NAMES ${APRUTIL_NAMES}
-  PATHS /usr/lib /usr/local/lib
+  PATHS /usr/lib /usr/local/lib /usr/local/apr/lib
   )
 
 IF (APRUTIL_LIBRARY AND APRUTIL_INCLUDE_DIR)
