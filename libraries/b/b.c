e01750c21058cc252128fda304c80ddb5492f712
Alexander Broekhuis <abroekhuis@apache.org>
2014-05-19 09:44:35 +0000

    CELIX-111, CELIX-112: Added example for multiple library support testing
    git-svn-id: https://svn.apache.org/repos/asf/incubator/celix/trunk@1595802 13f79535-47bb-0310-9956-ffa450edef68

---
+++ libraries/b/b.c
@@ -0,0 +1,33 @@
+/**
+ *Licensed to the Apache Software Foundation (ASF) under one
+ *or more contributor license agreements.  See the NOTICE file
+ *distributed with this work for additional information
+ *regarding copyright ownership.  The ASF licenses this file
+ *to you under the Apache License, Version 2.0 (the
+ *"License"); you may not use this file except in compliance
+ *with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing,
+ *software distributed under the License is distributed on an
+ *"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ *specific language governing permissions and limitations
+ *under the License.
+ */
+/*
+ * b.c
+ *
+ *  \date       31 Oct 2013
+ *  \author     <a href="mailto:celix-dev@incubator.apache.org">Apache Celix Project Team</a>
+ *  \copyright  Apache License, Version 2.0
+ */
+
+#include <stdio.h>
+
+#include "b.h"
+
+void sayBye(char *bye) {
+    printf("Bye: %s\n", bye);
+}
