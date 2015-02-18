4c714ca5472ba3574a732a3b4c5883f208584d8b
Alexander Broekhuis <abroekhuis@apache.org>
2015-02-18 12:02:07 +0000

    CELIX-90: Updated the CMake build options for remote services.
    - Moved examples to a dedicated sub-directory
    - Added options for all parts (topo manager, http rsa etc)
    - Renamed options to have shorter names
    
    git-svn-id: https://svn.apache.org/repos/asf/celix/trunk@1660605 13f79535-47bb-0310-9956-ffa450edef68

--- remote_services/examples/deploy.cmake
+++ remote_services/examples/deploy.cmake
@@ -14,36 +14,38 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-is_enabled(REMOTE_SERVICE_ADMIN)
-if (REMOTE_SERVICE_ADMIN)
-    deploy("remote-services-cfg" BUNDLES discovery_configured topology_manager remote_service_admin_http calculator shell shell_tui log_service log_writer
-                                 ENDPOINTS 
-                                    org.apache.celix.calc.api.Calculator_endpoint
-                                    org.apache.celix.calc.api.Calculator2_endpoint)
-    deploy("remote-services-cfg-client" BUNDLES topology_manager remote_service_admin_http shell shell_tui log_service log_writer calculator_shell discovery_configured
-                                        ENDPOINTS org.apache.celix.calc.api.Calculator_proxy org.apache.celix.calc.api.Calculator2_proxy)
+is_enabled(RSA_EXAMPLES)
+if (RSA_EXAMPLES)
+    is_enabled(RSA_REMOTE_SERVICE_ADMIN_HTTP)
+    if (RSA_REMOTE_SERVICE_ADMIN_HTTP)
+        is_enabled(RSA_DISCOVERY_CONFIGURED)
+        if (RSA_DISCOVERY_CONFIGURED)
+            deploy("remote-services-cfg" BUNDLES discovery_configured topology_manager remote_service_admin_http calculator shell shell_tui log_service log_writer
+                                         ENDPOINTS
+                                            org.apache.celix.calc.api.Calculator_endpoint
+                                            org.apache.celix.calc.api.Calculator2_endpoint)
+            deploy("remote-services-cfg-client" BUNDLES topology_manager remote_service_admin_http shell shell_tui log_service log_writer calculator_shell discovery_configured
+                                                ENDPOINTS org.apache.celix.calc.api.Calculator_proxy org.apache.celix.calc.api.Calculator2_proxy)
+        endif ()
+    endif ()
 
-	#TODO for remote-service-client the discovery should be added as last. If this is not done, 
-	#discovery will discover services before the topology manager is registered as 
-	#endpoint listener and services will be lost. This needs further study.
-
-    is_enabled(RSA_BUNDLES_REMOTE_SERVICE_ADMIN_SHM)
-    if (RSA_BUNDLES_REMOTE_SERVICE_ADMIN_SHM)
-        is_enabled(RSA_BUNDLES_DISCOVERY_SHM)
-        if (RSA_BUNDLES_DISCOVERY_SHM)
+    is_enabled(RSA_REMOTE_SERVICE_ADMIN_SHM)
+    if (RSA_REMOTE_SERVICE_ADMIN_SHM)
+        is_enabled(RSA_DISCOVERY_SHM)
+        if (RSA_DISCOVERY_SHM)
             deploy("remote-services-shm" BUNDLES discovery_shm topology_manager remote_service_admin_shm calculator shell shell_tui log_service log_writer 
                                          ENDPOINTS org.apache.celix.calc.api.Calculator_endpoint)
             deploy("remote-services-shm-client" BUNDLES topology_manager remote_service_admin_shm shell shell_tui log_service log_writer calculator_shell discovery_shm
                                                 ENDPOINTS org.apache.celix.calc.api.Calculator_proxy)
-        endif (RSA_BUNDLES_DISCOVERY_SHM)
-    endif (RSA_BUNDLES_REMOTE_SERVICE_ADMIN_SHM)
+        endif ()
+    endif ()
 
-    is_enabled(RSA_BUNDLES_DISCOVERY_ETCD)
-    if (RSA_BUNDLES_DISCOVERY_ETCD)
+    is_enabled(RSA_DISCOVERY_ETCD)
+    if (RSA_DISCOVERY_ETCD)
         deploy("remote-services-etcd" BUNDLES discovery_etcd topology_manager remote_service_admin_http calculator shell shell_tui log_service log_writer
                                      ENDPOINTS org.apache.celix.calc.api.Calculator_endpoint)
         deploy("remote-services-etcd-client" BUNDLES topology_manager remote_service_admin_http shell shell_tui log_service log_writer calculator_shell discovery_etcd
                                             ENDPOINTS org.apache.celix.calc.api.Calculator_proxy)
-    endif (RSA_BUNDLES_DISCOVERY_ETCD)
+    endif ()
 
-endif (REMOTE_SERVICE_ADMIN)
+endif (RSA_EXAMPLES)
