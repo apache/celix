39751029394b81caafe0395a4b5aea75dd07da7b
Bjoern Petri <bpetri@apache.org>
2015-10-07 09:51:02 +0200

    CELIX-267: add additional check whether endpoint is available

--- remote_services/remote_service_admin_http/private/src/remote_service_admin_impl.c
+++ remote_services/remote_service_admin_http/private/src/remote_service_admin_impl.c
@@ -322,7 +322,7 @@ static int remoteServiceAdmin_callback(struct mg_connection *conn) {
 				for (expIt = 0; expIt < arrayList_size(exports); expIt++) {
 					export_registration_pt export = arrayList_get(exports, expIt);
 					long serviceId = atol(service);
-					if (serviceId == export->endpointDescription->serviceId) {
+					if (serviceId == export->endpointDescription->serviceId && export->endpoint != NULL) {
 						uint64_t datalength = request_info->content_length;
 						char* data = malloc(datalength + 1);
 						mg_read(conn, data, datalength);
@@ -447,6 +447,7 @@ celix_status_t remoteServiceAdmin_exportService(remote_service_admin_pt admin, c
 				exportRegistration_open(registration);
 				exportRegistration_startTracking(registration);
 			}
+
 			celixThreadMutex_lock(&admin->exportedServicesLock);
 			hashMap_put(admin->exportedServices, reference, *registrations);
 			celixThreadMutex_unlock(&admin->exportedServicesLock);
