b815e68fc4ad5a8a7e1a11fb58a482d0b6c993ac
Pepijn Noltes <pepijnnoltes@gmail.com>
2016-02-04 16:26:18 +0100

    CELIX-343: Update config_admin examples to print updated properties.

--- config_admin/example/private/src/example.c
+++ config_admin/example/private/src/example.c
@@ -2,6 +2,8 @@
 
 #include <pthread.h>
 
+#include "hash_map.h"
+
 #include "example.h"
 
 struct example {
@@ -42,5 +44,16 @@ celix_status_t example_stop(example_pt component) {
 
 celix_status_t example_updated(example_pt component, properties_pt updatedProperties) {
     printf("updated called\n");
+    hash_map_pt map = (hash_map_pt)updatedProperties;
+    if (map != NULL) {
+        hash_map_iterator_pt iter = hashMapIterator_create(map);
+        while(hashMapIterator_hasNext(iter)) {
+            char *key = hashMapIterator_nextKey(iter);
+            char *value = properties_get(updatedProperties, key);
+            printf("got property %s:%s\n", key, value);
+        }
+    } else {
+        printf("updated with NULL properties\n");
+    }
     return CELIX_SUCCESS;
 }
