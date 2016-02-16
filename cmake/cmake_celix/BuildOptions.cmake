c1078421b70a25db60a9f99d3157198f5aadbe9c
Pepijn Noltes <pepijnnoltes@gmail.com>
2016-02-16 16:51:03 +0100

    CELIX-347: Updated BuildOptions to allow undefs in shared libaries. This is needed when using the asan

--- cmake/cmake_celix/BuildOptions.cmake
+++ cmake/cmake_celix/BuildOptions.cmake
@@ -2,6 +2,6 @@
 option(ENABLE_ADDRESS_SANITIZER "Enabled building with address sanitizer. Note for gcc libasan must be installed" OFF)
 
 if (ENABLE_ADDRESS_SANITIZER)
-    set(CMAKE_C_FLAGS "-lasan -fsanitize=address ${CMAKE_C_FLAGS}")
-    set(CMAKE_CXX_FLAGS "-lasan -fsanitize=address ${CMAKE_CXX_FLAGS}")
+    set(CMAKE_C_FLAGS "-lasan -fsanitize=address ${CMAKE_C_FLAGS} -Wl,--allow-shlib-undefined")
+    set(CMAKE_CXX_FLAGS "-lasan -fsanitize=address ${CMAKE_CXX_FLAGS} -Wl,--allow-shlib-undefined")
 endif()
