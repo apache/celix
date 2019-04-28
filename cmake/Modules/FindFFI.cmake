2d570596613a5f367af47912ce3ade43731c3d5f
Pepijn Noltes <pepijnnoltes@gmail.com>
2019-04-28 16:20:17 +0200

    CELIX-426: Update FindFFI so that it can find libffi installed with brew on OSX systems.
    Also updated the FindFFI to create an imported target (ffi::lib) for libffi.

--- cmake/Modules/FindFFI.cmake
+++ cmake/Modules/FindFFI.cmake
@@ -20,38 +20,61 @@
 #
 # Once done this will define
 #  FFI_FOUND - System has libffi
-#  FFI_INCLUDE_DIRS - The package include directories
-#  FFI_LIBRARIES - The libraries needed to use this package
+#  ffi::lib - Imported target for the libffi
 
-if (NOT WIN32)
-   # try using pkg-config to get the directories and then use these values
-   # in the find_path() and find_library() calls
-   find_package(PkgConfig QUIET)
-   PKG_CHECK_MODULES(PC_LIBFFI QUIET libffi)
+# try using pkg-config if available
+find_package(PkgConfig QUIET)
+
+if (PkgConfig_FOUND)
+    if (APPLE)
+        #set brew location for pkg-config
+        set(ENV{PKG_CONFIG_PATH} "/usr/local/opt/libffi/lib/pkgconfig:$ENV{PKG_CONFIG_PATH}")
+    endif ()
+    pkg_check_modules(LIBFFI REQUIRED libffi)
+
+    #use found LIBFFI pkg config info to search for the abs path fo the libffi lib.
+    #Note that the abs path info  is not present in the pkg-config results (only the -l and -L arguments)
+    find_library(FFI_ABS_LIB NAMES ffi PATHS ${LIBFFI_LIBRARY_DIRS} NO_DEFAULT_PATH NO_PACKAGE_ROOT_PATH NO_CMAKE_PATH NO_CMAKE_SYSTEM_PATH)
+    if (NOT FFI_ABS_LIB)
+        message(FATAL_ERROR "Cannot find abs path of libffi based on pkgconfig results")
+    endif ()
+
+    add_library(ffi::lib SHARED IMPORTED)
+    set_target_properties(ffi::lib PROPERTIES
+        IMPORTED_LOCATION "${FFI_ABS_LIB}"
+        INTERFACE_INCLUDE_DIRECTORIES "${LIBFFI_INCLUDE_DIRS}"
+    )
+
+    unset(FFI_ABS_LIB)
+else()
+    mark_as_advanced(GNUTLS_INCLUDE_DIR GNUTLS_LIBRARY)
+
+    find_library(FFI_LIBRARY NAMES ffi libffi
+            PATHS $ENV{FFI_DIR} ${FFI_DIR} /usr /usr/local /opt/local
+            PATH_SUFFIXES lib lib64 x86_64-linux-gnu lib/x86_64-linux-gnu
+            HINTS ${PC_LIBFFI_LIBDIR} ${PC_LIBFFI_LIBRARY_DIRS}
+    )
+
+    find_path(FFI_INCLUDE_DIR ffi.h
+            PATHS $ENV{FFI_DIR} ${FFI_DIR} /usr /usr/local /opt/local
+            PATH_SUFFIXES include include/ffi include/x86_64-linux-gnu x86_64-linux-gnu
+            HINTS ${PC_LIBFFI_INCLUDEDIR} ${PC_LIBFFI_INCLUDE_DIRS}
+    )
+
+    include(FindPackageHandleStandardArgs)
+    # handle the QUIETLY and REQUIRED arguments and set FFI_FOUND to TRUE
+    # if all listed variables are TRUE
+    find_package_handle_standard_args(FFI  DEFAULT_MSG
+                                      FFI_LIBRARY FFI_INCLUDE_DIR)
+    mark_as_advanced(FFI_INCLUDE_DIR FFI_LIBRARY)
+
+    if(FFI_FOUND)
+        set(FFI_LIBRARIES ${FFI_LIBRARY})
+        set(FFI_INCLUDE_DIRS ${FFI_INCLUDE_DIR})
+        add_library(ffi::lib SHARED IMPORTED)
+        set_target_properties(ffi::lib PROPERTIES
+                IMPORTED_LOCATION "${FFI_LIBRARY}"
+                INTERFACE_INCLUDE_DIRECTORIES "${FFI_INCLUDE_DIR}"
+        )
+    endif()
 endif ()
-
-mark_as_advanced(GNUTLS_INCLUDE_DIR GNUTLS_LIBRARY)
-
-find_library(FFI_LIBRARY NAMES ffi libffi
-        PATHS $ENV{FFI_DIR} ${FFI_DIR} /usr /usr/local /opt/local
-        PATH_SUFFIXES lib lib64 x86_64-linux-gnu lib/x86_64-linux-gnu
-        HINTS ${PC_LIBFFI_LIBDIR} ${PC_LIBFFI_LIBRARY_DIRS}
-)
-
-find_path(FFI_INCLUDE_DIR ffi.h
-		PATHS $ENV{FFI_DIR} ${FFI_DIR} /usr /usr/local /opt/local 
-        PATH_SUFFIXES include include/ffi include/x86_64-linux-gnu x86_64-linux-gnu
-        HINTS ${PC_LIBFFI_INCLUDEDIR} ${PC_LIBFFI_INCLUDE_DIRS}
-)
-
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set FFI_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(FFI  DEFAULT_MSG
-                                  FFI_LIBRARY FFI_INCLUDE_DIR)
-mark_as_advanced(FFI_INCLUDE_DIR FFI_LIBRARY) 
-
-if(FFI_FOUND)
-	set(FFI_LIBRARIES ${FFI_LIBRARY})
-	set(FFI_INCLUDE_DIRS ${FFI_INCLUDE_DIR})
-endif()
