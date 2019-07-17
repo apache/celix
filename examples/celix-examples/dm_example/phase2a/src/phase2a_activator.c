9c1b9a26077896ec39ca7967ccf64b881e06f986
Roy Lenferink <lenferinkroy@gmail.com>
2019-07-17 18:53:53 +0200

    Updated celix-examples to use celix_properties_t* over properties_pt
    Updated formatting as well

--- examples/celix-examples/dm_example/phase2a/src/phase2a_activator.c
+++ examples/celix-examples/dm_example/phase2a/src/phase2a_activator.c
@@ -1,20 +1,20 @@
 /**
- *Licensed to the Apache Software Foundation (ASF) under one
- *or more contributor license agreements.  See the NOTICE file
- *distributed with this work for additional information
- *regarding copyright ownership.  The ASF licenses this file
- *to you under the Apache License, Version 2.0 (the
- *"License"); you may not use this file except in compliance
- *with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *Unless required by applicable law or agreed to in writing,
- *software distributed under the License is distributed on an
- *"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- *specific language governing permissions and limitations
- *under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 #include <stdlib.h>
@@ -28,51 +28,51 @@
 
 struct phase2a_activator_struct {
     phase2a_cmp_t *phase2aCmp;
-	phase2_t phase2Serv;
+    phase2_t phase2Serv;
 };
 
 static celix_status_t activator_start(struct phase2a_activator_struct *act, celix_bundle_context_t *ctx) {
-	printf("phase2a: start\n");
-	celix_status_t status = CELIX_SUCCESS;
-	act->phase2aCmp = phase2a_create();
-	if (act->phase2aCmp != NULL) {
+    printf("phase2a: start\n");
+    celix_status_t status = CELIX_SUCCESS;
+    act->phase2aCmp = phase2a_create();
+    if (act->phase2aCmp != NULL) {
 
-		act->phase2Serv.handle = act->phase2aCmp;
-		act->phase2Serv.getData = (void *)phase2a_getData;
+        act->phase2Serv.handle = act->phase2aCmp;
+        act->phase2Serv.getData = (void *)phase2a_getData;
 
-		celix_properties_t *props = properties_create();
-		properties_set(props, "id", "phase2a");
+        celix_properties_t *props = celix_properties_create();
+        celix_properties_set(props, "id", "phase2a");
 
-		celix_dm_component_t *cmp = celix_dmComponent_create(ctx, "PHASE2A_PROCESSING_COMPONENT");
-		celix_dmComponent_setImplementation(cmp, act->phase2aCmp);
-		CELIX_DMCOMPONENT_SETCALLBACKS(cmp, phase2a_cmp_t *, phase2a_init, phase2a_start, phase2a_stop, phase2a_deinit);
-		celix_dmComponent_addInterface(cmp, PHASE2_NAME, PHASE2_VERSION, &act->phase2Serv, props);
+        celix_dm_component_t *cmp = celix_dmComponent_create(ctx, "PHASE2A_PROCESSING_COMPONENT");
+        celix_dmComponent_setImplementation(cmp, act->phase2aCmp);
+        CELIX_DMCOMPONENT_SETCALLBACKS(cmp, phase2a_cmp_t *, phase2a_init, phase2a_start, phase2a_stop, phase2a_deinit);
+        celix_dmComponent_addInterface(cmp, PHASE2_NAME, PHASE2_VERSION, &act->phase2Serv, props);
 
 
-		celix_dm_service_dependency_t *dep = celix_dmServiceDependency_create();
+        celix_dm_service_dependency_t *dep = celix_dmServiceDependency_create();
         celix_dmServiceDependency_setService(dep, PHASE1_NAME, PHASE1_RANGE_ALL, NULL);
         celix_dmServiceDependency_setCallback(dep, (void*)phase2a_setPhase1);
         celix_dmServiceDependency_setStrategy(dep, DM_SERVICE_DEPENDENCY_STRATEGY_LOCKING);
         celix_dmServiceDependency_setRequired(dep, true);
-		celix_dmComponent_addServiceDependency(cmp, dep);
+        celix_dmComponent_addServiceDependency(cmp, dep);
 
-		celix_dependency_manager_t *mng = celix_bundleContext_getDependencyManager(ctx);
-		celix_dependencyManager_add(mng, cmp);
-	} else {
-		status = CELIX_ENOMEM;
-	}
+        celix_dependency_manager_t *mng = celix_bundleContext_getDependencyManager(ctx);
+        celix_dependencyManager_add(mng, cmp);
+    } else {
+        status = CELIX_ENOMEM;
+    }
 
-	return status;
+    return status;
 }
 
 static celix_status_t activator_stop(struct phase2a_activator_struct *act, celix_bundle_context_t *ctx) {
-	printf("phase2a: stop\n");
+    printf("phase2a: stop\n");
     celix_dependency_manager_t *mng = celix_bundleContext_getDependencyManager(ctx);
     celix_dependencyManager_removeAllComponents(mng);
-	if (act->phase2aCmp != NULL) {
-		phase2a_destroy(act->phase2aCmp);
-	}
-	return CELIX_SUCCESS;
+    if (act->phase2aCmp != NULL) {
+        phase2a_destroy(act->phase2aCmp);
+    }
+    return CELIX_SUCCESS;
 }
 
 CELIX_GEN_BUNDLE_ACTIVATOR(struct phase2a_activator_struct, activator_start, activator_stop)
--- examples/celix-examples/log_service_example/src/activator.c
+++ examples/celix-examples/log_service_example/src/activator.c
@@ -1,20 +1,20 @@
 /**
- *Licensed to the Apache Software Foundation (ASF) under one
- *or more contributor license agreements.  See the NOTICE file
- *distributed with this work for additional information
- *regarding copyright ownership.  The ASF licenses this file
- *to you under the Apache License, Version 2.0 (the
- *"License"); you may not use this file except in compliance
- *with the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *Unless required by applicable law or agreed to in writing,
- *software distributed under the License is distributed on an
- *"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- *specific language governing permissions and limitations
- *under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 #include <stdlib.h>
@@ -25,23 +25,23 @@
 #include "log_helper.h"
 
 typedef struct user_data {
-	pthread_t logger_thread;
-	pthread_mutex_t lock;
-	bool running;
-	log_helper_pt log_helper;
+    pthread_t logger_thread;
+    pthread_mutex_t lock;
+    bool running;
+    log_helper_pt log_helper;
 } user_data_t;
 
 static void *loggerThread(void *userData);
 
 celix_status_t activator_start(user_data_t *data, celix_bundle_context_t *ctx) {
-	celix_status_t status = logHelper_create(ctx, &data->log_helper);
-	if (status == CELIX_SUCCESS) {
-		logHelper_start(data->log_helper);
-		data->running = true;
-		pthread_mutex_init(&data->lock, NULL);
-		pthread_create(&data->logger_thread, NULL, loggerThread, data);
-	}
-	return status;
+    celix_status_t status = logHelper_create(ctx, &data->log_helper);
+    if (status == CELIX_SUCCESS) {
+        logHelper_start(data->log_helper);
+        data->running = true;
+        pthread_mutex_init(&data->lock, NULL);
+        pthread_create(&data->logger_thread, NULL, loggerThread, data);
+    }
+    return status;
 }
 
 celix_status_t activator_stop(user_data_t *data, celix_bundle_context_t *ctx __attribute__((unused))) {
@@ -49,24 +49,24 @@ celix_status_t activator_stop(user_data_t *data, celix_bundle_context_t *ctx __a
     data->running = false;
     pthread_mutex_unlock(&data->lock);
     pthread_join(data->logger_thread, NULL);
-	logHelper_stop(data->log_helper);
-	logHelper_destroy(&data->log_helper);
-	return CELIX_SUCCESS;
+    logHelper_stop(data->log_helper);
+    logHelper_destroy(&data->log_helper);
+    return CELIX_SUCCESS;
 }
 
 
 static void *loggerThread(void *userData) {
-	user_data_t *data = userData;
+    user_data_t *data = userData;
 
     bool running = true;
-	while (running) {
-		logHelper_log(data->log_helper, OSGI_LOGSERVICE_INFO, "My log message");
-		sleep(1);
+    while (running) {
+        logHelper_log(data->log_helper, OSGI_LOGSERVICE_INFO, "My log message");
+        sleep(1);
         pthread_mutex_lock(&data->lock);
         running = data->running;
         pthread_mutex_unlock(&data->lock);
-	}
-	return NULL;
+    }
+    return NULL;
 }
 
 CELIX_GEN_BUNDLE_ACTIVATOR(user_data_t, activator_start, activator_stop)
