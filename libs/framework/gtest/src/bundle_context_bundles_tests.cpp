aad0ecd8a173bcea08c2d57f6d4442bc116fbcbc
Pepijn Noltes <pepijnnoltes@gmail.com>
2020-03-06 11:35:12 +0100

    Refactors some celix framework test from cpputest to gtest.

--- libs/framework/gtest/src/bundle_context_bundles_tests.cpp
+++ libs/framework/gtest/src/bundle_context_bundles_tests.cpp
@@ -17,6 +17,8 @@
  * under the License.
  */
 
+#include <gtest/gtest.h>
+
 #include <thread>
 #include <chrono>
 #include <iostream>
@@ -27,14 +29,11 @@
 
 #include "celix_api.h"
 
-#include <CppUTest/TestHarness.h>
-#include <CppUTest/CommandLineTestRunner.h>
-
-
-TEST_GROUP(CelixBundleContextBundlesTests) {
-    framework_t* fw = nullptr;
-    bundle_context_t *ctx = nullptr;
-    properties_t *properties = nullptr;
+class CelixBundleContextBundlesTests : public ::testing::Test {
+public:
+    celix_framework_t* fw = nullptr;
+    celix_bundle_context_t *ctx = nullptr;
+    celix_properties_t *properties = nullptr;
 
     const char * const TEST_BND1_LOC = "simple_test_bundle1.zip";
     const char * const TEST_BND2_LOC = "simple_test_bundle2.zip";
@@ -44,7 +43,7 @@ TEST_GROUP(CelixBundleContextBundlesTests) {
     const char * const TEST_BND_WITH_EXCEPTION_LOC = "bundle_with_exception.zip";
     const char * const TEST_BND_UNRESOLVEABLE_LOC = "unresolveable_bundle.zip";
 
-    void setup() {
+    CelixBundleContextBundlesTests() {
         properties = properties_create();
         properties_set(properties, "LOGHELPER_ENABLE_STDOUT_FALLBACK", "true");
         properties_set(properties, "org.osgi.framework.storage.clean", "onFirstInit");
@@ -53,35 +52,42 @@ TEST_GROUP(CelixBundleContextBundlesTests) {
         fw = celix_frameworkFactory_createFramework(properties);
         ctx = framework_getContext(fw);
     }
-
-    void teardown() {
+    
+    ~CelixBundleContextBundlesTests() override {
         celix_frameworkFactory_destroyFramework(fw);
     }
+
+    CelixBundleContextBundlesTests(CelixBundleContextBundlesTests&&) = delete;
+    CelixBundleContextBundlesTests(const CelixBundleContextBundlesTests&) = delete;
+    CelixBundleContextBundlesTests& operator=(CelixBundleContextBundlesTests&&) = delete;
+    CelixBundleContextBundlesTests& operator=(const CelixBundleContextBundlesTests&) = delete;
 };
 
-TEST(CelixBundleContextBundlesTests, installBundlesTest) {
+
+
+TEST_F(CelixBundleContextBundlesTests, installBundlesTest) {
     long bndId = celix_bundleContext_installBundle(ctx, "non-existing.zip", true);
-    CHECK(bndId < 0);
+    ASSERT_TRUE(bndId < 0);
 
     bndId = celix_bundleContext_installBundle(ctx, TEST_BND1_LOC, true);
-    CHECK(bndId >= 0);
+    ASSERT_TRUE(bndId >= 0);
 
     bndId = celix_bundleContext_installBundle(ctx, TEST_BND4_LOC, true); //not loaded in subdir
-    CHECK(bndId < 0);
+    ASSERT_TRUE(bndId < 0);
 
     setenv(CELIX_BUNDLES_PATH_NAME, "subdir", true);
     bndId = celix_bundleContext_installBundle(ctx, TEST_BND4_LOC, true); //subdir now part of CELIX_BUNDLES_PATH
-    CHECK(bndId >= 0);
+    ASSERT_TRUE(bndId >= 0);
 }
 
-TEST(CelixBundleContextBundlesTests, useBundlesTest) {
+TEST_F(CelixBundleContextBundlesTests, useBundlesTest) {
     int count = 0;
 
     auto use = [](void *handle, const bundle_t *bnd) {
         int *c = (int*)handle;
         *c += 1;
         long id = celix_bundle_getId(bnd);
-        CHECK(id >= 0);
+        ASSERT_TRUE(id >= 0);
     };
 
     celix_bundleContext_useBundles(ctx, &count, use);
@@ -89,193 +95,193 @@ TEST(CelixBundleContextBundlesTests, useBundlesTest) {
     count = 0;
     celix_bundleContext_installBundle(ctx, TEST_BND1_LOC, true);
     celix_bundleContext_useBundles(ctx, &count, use);
-    CHECK_EQUAL(1, count);
+    ASSERT_EQ(1, count);
 };
 
-TEST(CelixBundleContextBundlesTests, installAndUninstallBundlesTest) {
+TEST_F(CelixBundleContextBundlesTests, installAndUninstallBundlesTest) {
     //install bundles
     long bndId1 = celix_bundleContext_installBundle(ctx, TEST_BND1_LOC, true);
     long bndId2 = celix_bundleContext_installBundle(ctx, TEST_BND2_LOC, false);
     long bndId3 = celix_bundleContext_installBundle(ctx, TEST_BND3_LOC, true);
 
-    CHECK_TRUE(bndId1 >= 0L);
-    CHECK_TRUE(bndId2 >= 0L);
-    CHECK_TRUE(bndId3 >= 0L);
+    ASSERT_TRUE(bndId1 >= 0L);
+    ASSERT_TRUE(bndId2 >= 0L);
+    ASSERT_TRUE(bndId3 >= 0L);
 
-    CHECK_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId1));
-    CHECK_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId2));
-    CHECK_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId3));
+    ASSERT_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId1));
+    ASSERT_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId2));
+    ASSERT_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId3));
 
-    CHECK_TRUE(celix_bundleContext_isBundleActive(ctx, bndId1));
-    CHECK_FALSE(celix_bundleContext_isBundleActive(ctx, bndId2)); //not auto started
-    CHECK_TRUE(celix_bundleContext_isBundleActive(ctx, bndId3));
+    ASSERT_TRUE(celix_bundleContext_isBundleActive(ctx, bndId1));
+    ASSERT_FALSE(celix_bundleContext_isBundleActive(ctx, bndId2)); //not auto started
+    ASSERT_TRUE(celix_bundleContext_isBundleActive(ctx, bndId3));
 
     //uninstall bundles
-    CHECK_TRUE(celix_bundleContext_uninstallBundle(ctx, bndId1));
-    CHECK_TRUE(celix_bundleContext_uninstallBundle(ctx, bndId2));
-    CHECK_TRUE(celix_bundleContext_uninstallBundle(ctx, bndId3));
+    ASSERT_TRUE(celix_bundleContext_uninstallBundle(ctx, bndId1));
+    ASSERT_TRUE(celix_bundleContext_uninstallBundle(ctx, bndId2));
+    ASSERT_TRUE(celix_bundleContext_uninstallBundle(ctx, bndId3));
 
-    CHECK_FALSE(celix_bundleContext_isBundleInstalled(ctx, bndId1));
-    CHECK_FALSE(celix_bundleContext_isBundleInstalled(ctx, bndId2));
-    CHECK_FALSE(celix_bundleContext_isBundleInstalled(ctx, bndId3));
+    ASSERT_FALSE(celix_bundleContext_isBundleInstalled(ctx, bndId1));
+    ASSERT_FALSE(celix_bundleContext_isBundleInstalled(ctx, bndId2));
+    ASSERT_FALSE(celix_bundleContext_isBundleInstalled(ctx, bndId3));
 
-    CHECK_FALSE(celix_bundleContext_isBundleActive(ctx, bndId1)); //not uninstall -> not active
-    CHECK_FALSE(celix_bundleContext_isBundleActive(ctx, bndId2));
-    CHECK_FALSE(celix_bundleContext_isBundleActive(ctx, bndId3));
+    ASSERT_FALSE(celix_bundleContext_isBundleActive(ctx, bndId1)); //not uninstall -> not active
+    ASSERT_FALSE(celix_bundleContext_isBundleActive(ctx, bndId2));
+    ASSERT_FALSE(celix_bundleContext_isBundleActive(ctx, bndId3));
 
     //reinstall bundles
     long bndId4 = celix_bundleContext_installBundle(ctx, TEST_BND1_LOC, true);
     long bndId5 = celix_bundleContext_installBundle(ctx, TEST_BND2_LOC, false);
     long bndId6 = celix_bundleContext_installBundle(ctx, TEST_BND3_LOC, true);
 
-    CHECK_TRUE(bndId4 >= 0L);
-    CHECK_FALSE(bndId1 == bndId4); //not new id
-    CHECK_TRUE(bndId5 >= 0L);
-    CHECK_FALSE(bndId2 == bndId5); //not new id
-    CHECK_TRUE(bndId6 >= 0L);
-    CHECK_FALSE(bndId5 == bndId6); //not new id
+    ASSERT_TRUE(bndId4 >= 0L);
+    ASSERT_FALSE(bndId1 == bndId4); //not new id
+    ASSERT_TRUE(bndId5 >= 0L);
+    ASSERT_FALSE(bndId2 == bndId5); //not new id
+    ASSERT_TRUE(bndId6 >= 0L);
+    ASSERT_FALSE(bndId5 == bndId6); //not new id
 }
 
-TEST(CelixBundleContextBundlesTests, startBundleWithException) {
+TEST_F(CelixBundleContextBundlesTests, startBundleWithException) {
     long bndId = celix_bundleContext_installBundle(ctx, TEST_BND_WITH_EXCEPTION_LOC, true);
-    CHECK(bndId > 0); //bundle is installed, but not started
+    ASSERT_TRUE(bndId > 0); //bundle is installed, but not started
 
     bool called = celix_framework_useBundle(fw, false, bndId, nullptr, [](void */*handle*/, const celix_bundle_t *bnd) {
         auto state = celix_bundle_getState(bnd);
-        CHECK_EQUAL(state, OSGI_FRAMEWORK_BUNDLE_RESOLVED);
+        ASSERT_EQ(state, OSGI_FRAMEWORK_BUNDLE_RESOLVED);
     });
-    CHECK_TRUE(called);
+    ASSERT_TRUE(called);
 }
 
-TEST(CelixBundleContextBundlesTests, startUnresolveableBundle) {
+TEST_F(CelixBundleContextBundlesTests, startUnresolveableBundle) {
     long bndId = celix_bundleContext_installBundle(ctx, TEST_BND_UNRESOLVEABLE_LOC, true);
-    CHECK(bndId > 0); //bundle is installed, but not resolved
+    ASSERT_TRUE(bndId > 0); //bundle is installed, but not resolved
 
     bool called = celix_framework_useBundle(fw, false, bndId, nullptr, [](void *, const celix_bundle_t *bnd) {
         auto state = celix_bundle_getState(bnd);
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_INSTALLED, state);
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_INSTALLED, state);
     });
-    CHECK_TRUE(called);
+    ASSERT_TRUE(called);
 
     celix_bundleContext_startBundle(ctx, bndId);
 
    called = celix_framework_useBundle(fw, false, bndId, nullptr, [](void *, const celix_bundle_t *bnd) {
         auto state = celix_bundle_getState(bnd);
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_INSTALLED, state);
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_INSTALLED, state);
     });
-    CHECK_TRUE(called);
+    ASSERT_TRUE(called);
 }
 
 
-TEST(CelixBundleContextBundlesTests, useBundleTest) {
+TEST_F(CelixBundleContextBundlesTests, useBundleTest) {
     int count = 0;
 
     bool called = celix_bundleContext_useBundle(ctx, 0, &count, [](void *handle, const bundle_t *bnd) {
         int *c = (int*)handle;
         *c += 1;
         long id = celix_bundle_getId(bnd);
-        CHECK_EQUAL(0, id);
+        ASSERT_EQ(0, id);
     });
 
-    CHECK_TRUE(called);
-    CHECK_EQUAL(1, count);
+    ASSERT_TRUE(called);
+    ASSERT_EQ(1, count);
 };
 
-TEST(CelixBundleContextBundlesTests, StopStartTest) {
+TEST_F(CelixBundleContextBundlesTests, StopStartTest) {
     long bndId1 = celix_bundleContext_installBundle(ctx, TEST_BND1_LOC, true);
     long bndId2 = celix_bundleContext_installBundle(ctx, TEST_BND2_LOC, true);
     long bndId3 = celix_bundleContext_installBundle(ctx, TEST_BND3_LOC, true);
-    CHECK_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId1));
-    CHECK_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId2));
-    CHECK_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId3));
-    CHECK_FALSE(celix_bundleContext_isBundleInstalled(ctx, 600 /*non existing*/));
+    ASSERT_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId1));
+    ASSERT_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId2));
+    ASSERT_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId3));
+    ASSERT_FALSE(celix_bundleContext_isBundleInstalled(ctx, 600 /*non existing*/));
 
 
 
     celix_array_list_t *ids = celix_bundleContext_listBundles(ctx);
     size_t size = arrayList_size(ids);
-    CHECK_EQUAL(3, size);
+    ASSERT_EQ(3, size);
 
     int count = 0;
     celix_bundleContext_useBundles(ctx, &count, [](void *handle, const celix_bundle_t *bnd) {
         auto *c = (int*)handle;
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
         *c += 1;
     });
-    CHECK_EQUAL(3, count);
+    ASSERT_EQ(3, count);
 
 
     for (size_t i = 0; i < size; ++i) {
         bool stopped = celix_bundleContext_stopBundle(ctx, celix_arrayList_getLong(ids, (int)i));
-        CHECK_TRUE(stopped);
+        ASSERT_TRUE(stopped);
     }
 
     bool stopped = celix_bundleContext_stopBundle(ctx, 42 /*non existing*/);
-    CHECK_FALSE(stopped);
+    ASSERT_FALSE(stopped);
 
     bool started = celix_bundleContext_startBundle(ctx, 42 /*non existing*/);
-    CHECK_FALSE(started);
+    ASSERT_FALSE(started);
 
     for (size_t i = 0; i < size; ++i) {
-        bool started = celix_bundleContext_startBundle(ctx, celix_arrayList_getLong(ids, (int)i));
-        CHECK_TRUE(started);
+        started = celix_bundleContext_startBundle(ctx, celix_arrayList_getLong(ids, (int)i));
+        ASSERT_TRUE(started);
     }
 
     count = 0;
     celix_bundleContext_useBundles(ctx, &count, [](void *handle, const celix_bundle_t *bnd) {
         auto *c = (int*)handle;
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
         *c += 1;
     });
-    CHECK_EQUAL(3, count);
+    ASSERT_EQ(3, count);
 
     celix_arrayList_destroy(ids);
 }
 
-TEST(CelixBundleContextBundlesTests, DoubleStopTest) {
+TEST_F(CelixBundleContextBundlesTests, DoubleStopTest) {
     long bndId = celix_bundleContext_installBundle(ctx, TEST_BND1_LOC, true);
-    CHECK_TRUE(bndId > 0);
-    CHECK_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId));
+    ASSERT_TRUE(bndId > 0);
+    ASSERT_TRUE(celix_bundleContext_isBundleInstalled(ctx, bndId));
 
     bool called = celix_framework_useBundle(fw, false, bndId, nullptr, [](void *, const celix_bundle_t *bnd) {
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
     });
-    CHECK_TRUE(called);
+    ASSERT_TRUE(called);
 
     //first
     celix_bundleContext_stopBundle(ctx, bndId);
 
     called = celix_framework_useBundle(fw, false, bndId, nullptr, [](void *, const celix_bundle_t *bnd) {
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_RESOLVED, celix_bundle_getState(bnd));
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_RESOLVED, celix_bundle_getState(bnd));
     });
-    CHECK_TRUE(called);
+    ASSERT_TRUE(called);
 
     //second
     celix_bundleContext_stopBundle(ctx, bndId);
 
     called = celix_framework_useBundle(fw, false, bndId, nullptr, [](void *, const celix_bundle_t *bnd) {
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_RESOLVED, celix_bundle_getState(bnd));
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_RESOLVED, celix_bundle_getState(bnd));
     });
-    CHECK_TRUE(called);
+    ASSERT_TRUE(called);
 
     //first
     celix_bundleContext_startBundle(ctx, bndId);
 
     called = celix_framework_useBundle(fw, false, bndId, nullptr, [](void *, const celix_bundle_t *bnd) {
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
     });
-    CHECK_TRUE(called);
+    ASSERT_TRUE(called);
 
     //second
     celix_bundleContext_startBundle(ctx, bndId);
 
     called = celix_framework_useBundle(fw, false, bndId, nullptr, [](void *, const celix_bundle_t *bnd) {
-        CHECK_EQUAL(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
+        ASSERT_EQ(OSGI_FRAMEWORK_BUNDLE_ACTIVE, celix_bundle_getState(bnd));
     });
-    CHECK_TRUE(called);
+    ASSERT_TRUE(called);
 }
 
-TEST(CelixBundleContextBundlesTests, trackBundlesTest) {
+TEST_F(CelixBundleContextBundlesTests, trackBundlesTest) {
     struct data {
         int count{0};
         std::mutex mutex{};
@@ -285,7 +291,7 @@ TEST(CelixBundleContextBundlesTests, trackBundlesTest) {
 
     auto started = [](void *handle, const bundle_t *bnd) {
         struct data *d = static_cast<struct data*>(handle);
-        CHECK(bnd != nullptr);
+        ASSERT_TRUE(bnd != nullptr);
         d->mutex.lock();
         d->count += 1;
         d->cond.notify_all();
@@ -293,7 +299,7 @@ TEST(CelixBundleContextBundlesTests, trackBundlesTest) {
     };
     auto stopped = [](void *handle, const bundle_t *bnd) {
         struct data *d = static_cast<struct data*>(handle);
-        CHECK(bnd != nullptr);
+        ASSERT_TRUE(bnd != nullptr);
         d->mutex.lock();
         d->count -= 1;
         d->cond.notify_all();
@@ -301,28 +307,28 @@ TEST(CelixBundleContextBundlesTests, trackBundlesTest) {
     };
 
     long trackerId = celix_bundleContext_trackBundles(ctx, static_cast<void*>(&data), started, stopped);
-    CHECK_EQUAL(0, data.count); //note default framework bundle is not tracked
+    ASSERT_EQ(0, data.count); //note default framework bundle is not tracked
 
 
     long bundleId1 = celix_bundleContext_installBundle(ctx, TEST_BND1_LOC, true);
-    CHECK(bundleId1 >= 0);
+    ASSERT_TRUE(bundleId1 >= 0);
 
     {
         std::unique_lock<std::mutex> lock{data.mutex};
         data.cond.wait_for(lock, std::chrono::milliseconds(100), [&]{return data.count == 2;});
 
     }
-    CHECK_EQUAL(1, data.count);
+    ASSERT_EQ(1, data.count);
 
 
     long bundleId2 = celix_bundleContext_installBundle(ctx, TEST_BND2_LOC, true);
-    CHECK(bundleId2 >= 0);
+    ASSERT_TRUE(bundleId2 >= 0);
     {
         std::unique_lock<std::mutex> lock{data.mutex};
         data.cond.wait_for(lock, std::chrono::milliseconds(100), [&]{return data.count == 3;});
 
     }
-    CHECK_EQUAL(2, data.count);
+    ASSERT_EQ(2, data.count);
 
     celix_bundleContext_uninstallBundle(ctx, bundleId2);
     {
@@ -330,31 +336,31 @@ TEST(CelixBundleContextBundlesTests, trackBundlesTest) {
         data.cond.wait_for(lock, std::chrono::milliseconds(100), [&]{return data.count == 2;});
 
     }
-    CHECK_EQUAL(2, data.count);
+    ASSERT_EQ(2, data.count);
 
     long bundleId3 = celix_bundleContext_installBundle(ctx, TEST_BND3_LOC, true);
-    CHECK(bundleId3 >= 0);
+    ASSERT_TRUE(bundleId3 >= 0);
     {
         std::unique_lock<std::mutex> lock{data.mutex};
         data.cond.wait_for(lock, std::chrono::milliseconds(100), [&]{return data.count == 3;});
 
     }
-    CHECK_EQUAL(3, data.count);
+    ASSERT_EQ(3, data.count);
 
     bundleId2 = celix_bundleContext_installBundle(ctx, TEST_BND2_LOC, true);
-    CHECK(bundleId3 >= 0);
+    ASSERT_TRUE(bundleId3 >= 0);
     {
         std::unique_lock<std::mutex> lock{data.mutex};
         data.cond.wait_for(lock, std::chrono::milliseconds(100), [&]{return data.count == 4;});
 
     }
-    CHECK_EQUAL(4, data.count);
+    ASSERT_EQ(4, data.count);
 
 
     celix_bundleContext_stopTracker(ctx, trackerId);
 };
 
-TEST(CelixBundleContextBundlesTests, useBundlesConcurrentTest) {
+TEST_F(CelixBundleContextBundlesTests, useBundlesConcurrentTest) {
 
     struct data {
         std::mutex mutex{};
@@ -365,7 +371,7 @@ TEST(CelixBundleContextBundlesTests, useBundlesConcurrentTest) {
     struct data data{};
 
     auto use = [](void *handle, const bundle_t *bnd) {
-        CHECK(bnd != nullptr);
+        ASSERT_TRUE(bnd != nullptr);
 
         struct data *d = static_cast<struct data*>(handle);
 
@@ -380,7 +386,7 @@ TEST(CelixBundleContextBundlesTests, useBundlesConcurrentTest) {
 
     auto call = [&] {
         bool called = celix_bundleContext_useBundle(ctx, bndId, &data, use);
-        CHECK(called);
+        ASSERT_TRUE(called);
     };
     std::thread useThread{call};
 
@@ -413,7 +419,7 @@ TEST(CelixBundleContextBundlesTests, useBundlesConcurrentTest) {
     std::cout << "uninstall thread joined" << std::endl;
 };
 
-TEST(CelixBundleContextBundlesTests, bundleInfoTests) {
+TEST_F(CelixBundleContextBundlesTests, bundleInfoTests) {
     struct data {
         int provideCount{0};
         int requestedCount{0};
@@ -431,18 +437,18 @@ TEST(CelixBundleContextBundlesTests, bundleInfoTests) {
     };
 
     bool called = celix_bundleContext_useBundle(ctx, 0, &data, updateCountFp);
-    CHECK_TRUE(called);
-    CHECK_EQUAL(0, data.provideCount);
-    CHECK_EQUAL(0, data.requestedCount);
+    ASSERT_TRUE(called);
+    ASSERT_EQ(0, data.provideCount);
+    ASSERT_EQ(0, data.requestedCount);
 
 
     long svcId = celix_bundleContext_registerService(ctx, (void*)0x42, "NopService", NULL);
     long trackerId = celix_bundleContext_trackServices(ctx, "AService", NULL, NULL, NULL);
 
     called = celix_bundleContext_useBundle(ctx, 0, &data, updateCountFp);
-    CHECK_TRUE(called);
-    CHECK_EQUAL(1, data.provideCount);
-    CHECK_EQUAL(1, data.requestedCount);
+    ASSERT_TRUE(called);
+    ASSERT_EQ(1, data.provideCount);
+    ASSERT_EQ(1, data.requestedCount);
 
     celix_bundleContext_unregisterService(ctx, svcId);
     celix_bundleContext_stopTracker(ctx, trackerId);
