dc4971036d54418a41de1b1547ff3d902703841e
Alexander Broekhuis <alexander.broekhuis@luminis.eu>
2020-04-20 16:25:23 +0200

    Updated Config to use build type specific CelixTargets.

--- cmake/cmake_celix/BundlePackaging.cmake
+++ cmake/cmake_celix/BundlePackaging.cmake
@@ -107,7 +107,7 @@ Optional arguments are:
 - GROUP: The group the bundle is part of. This will be used as Bundle-Group manifest entry. Default this is empty (no group).
 - VERSION: The bundle version. This will be used for the Bundle-Version manifest entry. In combination with SOURCES the version will also be used to set the activator library target property VERSION and SOVERSION.
   For SOVERSION only the major part is used. Expected scheme is "<major>.<minor>.<path>". Default version is "0.0.0"
-- FILENAME: The filename of the bundle file. Default is <bundle_target_name>.zip.
+- FILENAME: The filename of the bundle file, without extension. Default is <bundle_target_name>.zip.
 - PRIVATE_LIBRARIES: private libraries to be included in the bundle. Specified libraries are added to the "Private-Library" manifest statement and added in the root of the bundle. libraries can be cmake library targets or absolute paths to existing libraries.
 - HEADERS: Additional headers values that are appended to the bundle manifest.
 
@@ -190,11 +190,11 @@ function(add_celix_bundle)
         set(BUNDLE_SYMBOLIC_NAME ${BUNDLE_TARGET_NAME})
     endif ()
 
-    if (NOT DEFINED BUNDLE_FILENAME AND NOT CMAKE_BUILD_TYPE STREQUAL "Release")
-        set(BUNDLE_FILENAME ${BUNDLE_TARGET_NAME}-${CMAKE_BUILD_TYPE}.zip)
-    elseif(NOT DEFINED BUNDLE_FILENAME)
-        set(BUNDLE_FILENAME ${BUNDLE_TARGET_NAME}.zip)
+    if (NOT DEFINED BUNDLE_FILENAME)
+        set(BUNDLE_FILENAME ${BUNDLE_TARGET_NAME})
     endif ()
+
+    set(BUNDLE_FILENAME ${BUNDLE_FILENAME}-${CMAKE_BUILD_TYPE}.zip)
 
     set(BUNDLE_FILE "${CMAKE_CURRENT_BINARY_DIR}/${BUNDLE_FILENAME}")
     #set(BUNDLE_CONTENT_DIR "${CMAKE_CURRENT_BINARY_DIR}/${BUNDLE_TARGET_NAME}_content")
