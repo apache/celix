859f9d56f6a306b0eabb96bbe06069b1d061c720
Pepijn Noltes <pepijnnoltes@gmail.com>
2021-05-28 17:17:09 +0200

    Fixes ser provider test suite

--- bundles/pubsub/pubsub_utils/gtest/src/PubSubSerializationProviderTestSuite.cc
+++ bundles/pubsub/pubsub_utils/gtest/src/PubSubSerializationProviderTestSuite.cc
@@ -48,14 +48,14 @@ public:
 
 TEST_F(PubSubSerializationProviderTestSuite, CreateDestroy) {
     //checks if the bundles are started and stopped correctly (no mem leaks).
-    auto* provider = pubsub_serializationProvider_create(ctx.get(), "test", 0, nullptr, nullptr, nullptr, nullptr);
+    auto* provider = pubsub_serializationProvider_create(ctx.get(), "test", false, 0, nullptr, nullptr, nullptr, nullptr);
     auto count = celix_bundleContext_useService(ctx.get(), PUBSUB_MESSAGE_SERIALIZATION_MARKER_NAME, nullptr, nullptr);
     EXPECT_EQ(1, count);
     pubsub_serializationProvider_destroy(provider);
 }
 
 TEST_F(PubSubSerializationProviderTestSuite, FindSerializationServices) {
-    auto* provider = pubsub_serializationProvider_create(ctx.get(), "test", 0, nullptr, nullptr, nullptr, nullptr);
+    auto* provider = pubsub_serializationProvider_create(ctx.get(), "test", false, 0, nullptr, nullptr, nullptr, nullptr);
 
     size_t nrEntries = pubsub_serializationProvider_nrOfEntries(provider);
     EXPECT_EQ(5, nrEntries);
