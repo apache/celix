292aa9b434835b469ec9dbcea048664c0cfb5335
Pepijn Noltes <pepijnnoltes@gmail.com>
2021-08-30 20:40:33 +0200

    Fixes some copy/paste errors and update documentation for libzip

--- cmake/Modules/FindLIBZIP.cmake
+++ cmake/Modules/FindLIBZIP.cmake
@@ -1,3 +1,4 @@
+# Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
 # regarding copyright ownership.  The ASF licenses this file
@@ -13,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
 
 # - Try to find libzip
 #
