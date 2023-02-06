cbb662edaf73cb2a0c0882bbdbacacef0faf15bf
PengZheng <howtofly@gmail.com>
2023-02-06 10:47:54 +0800

    Don't install googletest when installing Celix.

--- cmake/AddGTest.cmake
+++ cmake/AddGTest.cmake
@@ -16,6 +16,7 @@
 # under the License.
 
 include(FetchContent)
+set(INSTALL_GTEST FALSE CACHE BOOL "" FORCE)
 FetchContent_Declare(
         googletest
         GIT_REPOSITORY https://github.com/google/googletest.git
