55120acb429582dadd72a6c6c24fd5bb52c013ac
Pepijn Noltes <pnoltes@apache.org>
2023-06-25 21:13:34 +0200

    Increase sleep time to ensure timeout log message is printed for scheduled event test suite

--- libs/framework/gtest/src/ScheduledEventTestSuite.cc
+++ libs/framework/gtest/src/ScheduledEventTestSuite.cc
@@ -663,10 +663,10 @@ TEST_F(ScheduledEventTestSuite, ScheduledEventTimeoutLogTest) {
     opts.initialDelayInSeconds = 0.01;
     opts.intervalInSeconds = 0.01;
     opts.callback = [](void*){
-        std::this_thread::sleep_for(std::chrono::milliseconds{100});
+        std::this_thread::sleep_for(std::chrono::milliseconds{200});
     };
     opts.removeCallback = [](void*){
-        std::this_thread::sleep_for(std::chrono::milliseconds{100});
+        std::this_thread::sleep_for(std::chrono::milliseconds{200});
     };
     long eventId = celix_bundleContext_scheduleEvent(fw->getFrameworkBundleContext()->getCBundleContext(), &opts);
 
