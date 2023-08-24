697e7ede4b30406ff3851d54e5f4c1c31384f24f
PengZheng <howtofly@gmail.com>
2023-08-24 13:32:29 +0800

    Run gcov by GNU make.

--- conanfile.py
+++ conanfile.py
@@ -407,7 +407,7 @@ class CelixConan(ConanFile):
         self.validate()
 
     def generate(self):
-        tc = CMakeToolchain(self, generator="Ninja")
+        tc = CMakeToolchain(self)
         for opt in self._celix_defaults.keys():
             tc.cache_variables[opt.upper()] = self.options.get_safe(opt)
         if self.options.enable_testing:
