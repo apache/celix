1db9b858fe924e87e0290d812062636a14cd5c17
PengZheng <howtofly@gmail.com>
2024-03-06 19:16:52 +0800

    Fix copy error in generate method.

--- examples/conan_test_package/conanfile.py
+++ examples/conan_test_package/conanfile.py
@@ -25,7 +25,10 @@ class TestPackageConan(ConanFile):
     # requires = "celix/2.3.0@docker/test"
 
     def imports(self):
-        self.copy("*", src="@libdirs", dst="lib")
+        if self.settings.os == "Linux":
+            self.copy("*.so*", src="@libdirs", dst="lib")
+        elif self.settings.os == "Macos":
+            self.copy("*.dylib", src="@libdirs", dst="lib")
 
     def build(self):
         cmake = CMake(self)
