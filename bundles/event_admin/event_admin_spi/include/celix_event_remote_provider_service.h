b17188eab69be0ce55c4794717006e5bd82245ca
xuzhenbao <xuzhenbao133@163.com>
2024-09-05 22:02:11 +0800

    Add event_admin_spi

---
+++ bundles/event_admin/event_admin_spi/include/celix_event_remote_provider_service.h
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef CELIX_EVENT_REMOTE_PROVIDER_SERVICE_H
+#define CELIX_EVENT_REMOTE_PROVIDER_SERVICE_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "celix_properties.h"
+#include "celix_errno.h"
+
+/**
+ * @brief The Event Remote Provider service name
+ */
+#define CELIX_EVENT_REMOTE_PROVIDER_SERVICE_NAME "celix_event_remote_provider_service"
+
+/**
+ * @brief The Event Remote Provider service version
+ */
+#define CELIX_EVENT_REMOTE_PROVIDER_SERVICE_VERSION "1.0.0"
+#define CELIX_EVENT_REMOTE_PROVIDER_SERVICE_USE_RANGE "[1.0.0,2)"
+
+typedef struct celix_event_remote_provider_service {
+    void* handle;
+    /**
+     * @brief Asynchronous event delivery . This method returns to the caller before delivery of the event is completed.
+     * @param[in] handle The handle as provided by the service registration.
+     * @param[in] topic The topic of the event.
+     * @param[in] properties The properties of the event. It can be NULL.
+     * @return Status code indicating failure or success. CELIX_SUCCESS if no errors are encountered. If an error is encountered, it should be return celix errno.
+     */
+    celix_status_t (*postEvent)(void* handle, const char* topic, const celix_properties_t* properties);
+    /**
+     * @brief Synchronous event delivery. This method does not return to the caller until delivery of the event is completed.
+     * @param[in] handle The handle as provided by the service registration.
+     * @param[in] topic The topic of the event.
+     * @param[in] properties The properties of the event. It can be NULL.
+     * @return Status code indicating failure or success. CELIX_SUCCESS if no errors are encountered. If an error is encountered, it should be return celix errno.
+     */
+    celix_status_t (*sendEvent)(void* handle, const char* topic, const celix_properties_t* properties);
+}celix_event_remote_provider_service_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //CELIX_EVENT_REMOTE_PROVIDER_SERVICE_H
