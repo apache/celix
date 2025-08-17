14dfeef72aac23b3099654341c18da88e40f8942
Pepijn Noltes <pnoltes@apache.org>
2025-08-17 21:09:59 +0200

    Refactor global curl init in celix launcher

--- conanfile.py
+++ conanfile.py
@@ -369,6 +369,8 @@ class CelixConan(ConanFile):
                 tc.cache_variables["BUILD_ERROR_INJECTOR_JANSSON"] = "ON"
             if "mosquitto" in lst:
                 tc.cache_variables["BUILD_ERROR_INJECTOR_MOSQUITTO"] = "ON"
+            if "libcurl" in lst:
+                tc.cache_variables["BUILD_ERROR_INJECTOR_CURL"] = "ON"
         tc.cache_variables["CELIX_ERR_BUFFER_SIZE"] = str(self.options.celix_err_buffer_size)
         # tc.cache_variables["CMAKE_PROJECT_Celix_INCLUDE"] = os.path.join(self.build_folder, "conan_paths.cmake")
         # the following is workaround for https://github.com/conan-io/conan/issues/7192
