d62160cf6136c55f7b5dd4d51b1ec67b0a311cdc
PengZheng <howtofly@gmail.com>
2026-02-10 16:56:08 +0800

    Limit gcc analyzer to C.

--- cmake/celix_project/CelixProject.cmake
+++ cmake/celix_project/CelixProject.cmake
@@ -98,8 +98,8 @@ if (ENABLE_GCC_ANALYZER)
             "-Wno-analyzer-fd-leak" 
             "-Wno-analyzer-shift-count-negative"
         )  
-       add_compile_options(${ANALYZER_FLAGS})
-    else()  
+       add_compile_options("$<$<COMPILE_LANGUAGE:C>:${ANALYZER_FLAGS}>")
+    else()
         message(WARNING "ENABLE_GCC_ANALYZER is only supported with GCC ")  
     endif()  
 endif()
