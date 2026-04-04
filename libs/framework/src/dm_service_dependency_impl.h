562fd968e6c947cf653b7707a500a1a860471bf9
Deedss <gertjan.rolink@outlook.com>
2026-04-04 17:35:49 +0000

    - Fix formatting in changed files. - Add service count to printinfo

--- libs/framework/src/dm_service_dependency_impl.h
+++ libs/framework/src/dm_service_dependency_impl.h
@@ -59,8 +59,8 @@ struct celix_dm_service_dependency {
     long svcTrackerId;                 // active tracker id
     size_t nrOfActiveStoppingTrackers; // nr of async stop tracker still active (should be 0 or 1)
     size_t trackedSvcCount;
-    size_t minimalCardinality;         // minimal nr of service required for availability
-    void* callbackHandle; // This handle can be set to be used instead of the component implementation
+    size_t minimalCardinality; // minimal nr of service required for availability
+    void* callbackHandle;      // This handle can be set to be used instead of the component implementation
 };
 
 celix_status_t celix_dmServiceDependency_enable(celix_dm_service_dependency_t *dependency);
